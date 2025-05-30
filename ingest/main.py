5de95c85da4615b2c5ed9d479729d4d52708c220
noah213 <typicalfilmcrew@gmail.com>
2025-05-29 21:39:26 -0400

    working polling

--- ingest/main.py
+++ ingest/main.py
@@ -1,8 +1,26 @@
-import os, time, datetime as dt
+import os, time, httpx, json
 
-interval = int(os.getenv("HEARTBEAT_INTERVAL", "5"))
+CIK  = os.getenv("CIK").zfill(10) #key for the SEC
+URL  = f"https://data.sec.gov/submissions/CIK{CIK}.json" #url we get data from
+UA   = {"User-Agent": os.getenv("USER_AGENT")} # our user
+WAIT = int(os.getenv("POLL_INTERVAL", "600")) #  wait time
 
-if __name__ == "__main__":
-    while True:
-        print("💓  ingest container alive at", dt.datetime.utcnow().isoformat(), flush=True)
-        time.sleep(interval)
+seen: set[str] = set()
+
+while True:
+    try:
+        r = httpx.get(URL, headers=UA, timeout=30) #sending a get request
+        if r.status_code == 429: # sending too many request
+            print("SEC TIMEOUT")
+            time.sleep(60)
+            continue
+        r.raise_for_status()
+        newest = r.json()["filings"]["recent"]["accessionNumber"][0] #getting the first accession which will be the newest
+        if newest not in seen: 
+            print("Recieved new Filing: ", newest, flush=True)
+            seen.add(newest) # reporting and storing new filing for use 
+            
+    except Exception as e: #error checking 
+        print("polling error: ", e, flush=True)
+
+    time.sleep(WAIT) #waiting before checking for new DATA
